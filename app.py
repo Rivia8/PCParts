8c426b9f25101efeac0ace2d3ad14fd40c924224
Rivia8 <armaanahmed413@gmail.com>
2025-09-06 21:39:03 +0100

    configured search query, needs updating for the dictionary now returned by scraper.py

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from scraper import find_scan_prices
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    prices = []
+    query = ""
+
+    # Only runs when the user submits the form
+    if request.method == 'POST':
+        query = request.form.get('user_text', '')
+        if query:
+            # Returns a list of prices
+            prices = find_scan_prices(query)
+        
+    return render_template("index.html", prices = prices, query=query)
 
 if __name__ == "__main__":
     app.run(debug=True)
