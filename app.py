412a67f9ac45709d5044350e6a9805597ad4665a
Rivia8 <armaanahmed413@gmail.com>
2025-09-10 09:57:20 +0100

    now implemented update to display dicts in the website through a table

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 from flask import Flask, render_template, request
-from scraper import find_scan_prices
+from scan_scraper import find_scan_products
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    prices = []
+    products = []
     query = ""
 
     # Only runs when the user submits the form
     if request.method == 'POST':
         query = request.form.get('user_text', '')
         if query:
-            # Returns a list of prices
-            prices = find_scan_prices(query)
+            # Returns a list of products
+            products = find_scan_products(query)
         
-    return render_template("index.html", prices = prices, query=query)
+    return render_template("index.html", products = products, query=query)
 
 if __name__ == "__main__":
     app.run(debug=True)
