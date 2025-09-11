655fccd455bf628928f3c6f77e6794a91985bee9
Rivia8 <armaanahmed413@gmail.com>
2025-09-11 22:40:30 +0100

    used selenium to implement scraper for OverclockersUK due to JS rendering

--- overclockers_scraper.py
+++ overclockers_scraper.py
@@ -1,9 +1,10 @@
-from bs4 import BeautifulSoup
-import requests
+from time import sleep
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
-}
+from bs4 import BeautifulSoup
+
+from selenium import webdriver
+from selenium.webdriver import FirefoxOptions
+from selenium.webdriver.common.by import By
 
 '''URL configurator based on OverClockers querying'''
 def configure_oc_url(search_item):
@@ -15,4 +16,46 @@ def configure_oc_url(search_item):
     oc_url = 'https://www.overclockers.co.uk/?query=' + new_search
     return oc_url
 
-print(configure_oc_url('rtx 5080'))
+'''Produces a dictionary of products with product names, urls and price'''
+def find_oc_products(search_item):
+    
+    options = webdriver.FirefoxOptions()
+    # options.add_argument('--width=1920')
+    # options.add_argument('--height=1080')
+    options.add_argument("-headless")
+
+    driver = webdriver.Firefox(options = options) 
+    driver.get(configure_oc_url(search_item))
+
+    sleep(0.5)  # Sleep in order to wait for the cookie page to load
+    cookie_button = driver.find_element(By.ID, "CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll")
+    cookie_button.click()
+
+    sleep(2)
+    html = driver.page_source
+    oc_soup = BeautifulSoup(html, 'html.parser')
+    row_listings = oc_soup.find('div', class_='row row--listing js-hide--empty-search')
+    cols = row_listings.find_all('div', class_='col')
+
+    sleep(0.5)
+    products_list = []
+    base_url = 'https://www.overclockers.co.uk'
+    for col in cols:
+        try:
+            name_element = col.find('h6', class_='h5 lh-1-4 mb-0 text-break').find('a')
+            price_element = col.find('span', class_='price__amount')
+
+            if name_element and price_element:
+                product_name = name_element.text.strip()
+                product_url = base_url + name_element['href']
+                price = price_element.text.strip()
+
+                products_list.append({
+                    'name': product_name,
+                    'url': product_url,
+                    'price': price
+                })
+        except AttributeError:
+            continue
+    
+    return products_list
